391bc634947b55f36013fd65ca63d8687c0cfb5f
EzzBox210 <georgiy.gordeev2007@gmail.com>
2024-11-10 17:30:14 +0300

    Add files via upload

---
+++ Pingpong.py
@@ -0,0 +1,48 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+
+
+       #каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+
+
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+       class Player(GameSprite):
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+   def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+#игровая сцена:
+back = (200, 255, 255) #цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+#флаги, отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
